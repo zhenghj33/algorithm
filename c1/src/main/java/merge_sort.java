d2e95943ce70982b80895094c6be03fe916ace2f
zhenghj <3138684147@qq.com>
2023-07-20 18:07:27 +0800

    2还是归并排序

--- c1/src/main/java/merge_sort.java
+++ c1/src/main/java/merge_sort.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class merge_sort {
     static int N = 100010;
     static int[] tmp = new int[N];
@@ -15,13 +19,24 @@ public class merge_sort {
         int i = l, j = mid + 1;//i是左半边的起点，j是右半边的起点
         while (i <= mid && j <= r) {
             if (q[i] <= q[j]) {
-
+                tmp[k++] = q[i++];
+            } else {
+                tmp[k++] = q[j++];
             }
+        }
+        while (i <= mid) tmp[k++] = q[i++];//如果是左半边没有循环完，就直接把剩下的接到辅助数组后面
+        while (j <= r) tmp[k++] = q[j++];//如果是右半边没有循环完
+        //把辅助数组搬到q数组
+        for (i = l, j = 0; i <= r; i++, j++) {
+            q[i] = tmp[j];
         }
     }
 
     //输入输出
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] q = new int[n];
 
     }
 }

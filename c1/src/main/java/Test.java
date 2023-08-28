18bf704b454107554aa1905279c131d224bb759e
zhenghj <3138684147@qq.com>
2023-08-29 01:03:02 +0800

    习题练习

--- c1/src/main/java/Test.java
+++ c1/src/main/java/Test.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
 
 public class Test {
 
@@ -9,37 +10,7 @@ public class Test {
 
     //输入输出
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] s1 = br.readLine().split(" ");
-        int n = Integer.parseInt(s1[0]);
-        int q = Integer.parseInt(s1[1]);
-        String[] s2 = br.readLine().split(" ");
-        for (int i = 0; i < n; i++) {
-            a[i] = Integer.parseInt(s2[i]);
-        }
-        while (q-- > 0) {
-            int k = Integer.parseInt(br.readLine());
-            int l = 0, r = n - 1;
-            while (l < r) {
-                int mid = l + r >> 1;
-                if (a[mid] >= k) {
-                    r = mid;
-                } else l = mid + 1;
-            }
-            if (a[l] != k) {
-                System.out.println("-1 -1");
-            } else {
-                int left = l;
-                l = 0;
-                r = n - 1;
-                while (l < r) {
-                    int mid = l + r + 1 >> 1;
-                    if (a[mid] <= k) {
-                        l = mid;
-                    } else r = mid - 1;
-                }
-                System.out.println(left + " " + l);
-            }
-        }
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");//获取当前时间，精确到分
+        System.out.println();
     }
 }
---
+++ c1/src/main/java/no786.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 786. 第k个数
+ * <p>
+ * 给定一个长度为 n
+ * 的整数数列，以及一个整数 k
+ * ，请用快速选择算法求出数列从小到大排序后的第 k
+ * 个数。
+ * <p>
+ * 输入格式
+ * 第一行包含两个整数 n
+ * 和 k
+ * 。
+ * <p>
+ * 第二行包含 n
+ * 个整数（所有整数均在 1∼109
+ * 范围内），表示整数数列。
+ * <p>
+ * 输出格式
+ * 输出一个整数，表示数列的第 k
+ * 小数。
+ * <p>
+ * 数据范围
+ * 1≤n≤100000
+ * ,
+ * 1≤k≤n
+ * 输入样例：
+ * 5 3
+ * 2 4 1 5 3
+ * 输出样例：
+ * 3
+ */
+public class no786 {
+    public static void quickSort(int[] q, int l, int r) {
+        if (l >= r) return;
+        int x = q[l], i = l - 1, j = r + 1;
+        while (i < j) {
+            while (q[++i] < x) ;
+            while (q[--j] > x) ;
+            if (i < j) {
+                int t = q[i];
+                q[i] = q[j];
+                q[j] = t;
+            }
+        }
+        quickSort(q, l, j);
+        quickSort(q, j + 1, r);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s1 = br.readLine().split(" ");
+        int n = Integer.parseInt(s1[0]);
+        int k = Integer.parseInt(s1[1]);
+        String[] s2 = br.readLine().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(s2[i]);//需要排序的数组
+        }
+        quickSort(a, 0, a.length - 1);
+        //打印结果
+        System.out.println(a[k-1]);
+    }
+
+}

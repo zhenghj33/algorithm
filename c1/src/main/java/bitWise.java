40fb80920e1a2551d1e9baf2ae77c431215e4e0d
zhenghj <3138684147@qq.com>
2023-09-17 18:18:57 +0800

    求二进制中1的个数

--- c1/src/main/java/bitWise.java
+++ c1/src/main/java/bitWise.java
@@ -6,18 +6,28 @@ import java.io.InputStreamReader;
  * 位运算
  */
 public class bitWise {
-    static int[] a = new int[100010];//原数组
+    static int[] a = new int[100100];
 
+    // 输入输出
     public static void main(String[] args) throws IOException {
-        //输入
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s1 = br.readLine();
         int n = Integer.parseInt(s1);
         String[] s2 = br.readLine().split(" ");
         for (int i = 0; i < n; i++) {
+            int count = 0;
             a[i] = Integer.parseInt(s2[i]);
+            while (a[i] != 0) {
+                a[i] -= lowbit(a[i]);// 二进制相减
+                count++;
+            }
+            System.out.print(count + " ");
         }
+    }
 
-
+    // 能够返回二进制的最后一个1
+    public static int lowbit(int x) {
+        // & 按位与操作符，同时为1是1
+        return -x & x;
     }
 }

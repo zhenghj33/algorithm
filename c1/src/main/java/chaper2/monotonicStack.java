ea0cf8bb089a6448e0a14e8b16cfe761f56c2344
zhenghj <3138684147@qq.com>
2023-09-21 01:14:55 +0800

    用数组模拟队列

---
+++ c1/src/main/java/chaper2/monotonicStack.java
@@ -0,0 +1,4 @@
+package chaper2;
+
+public class monotonicStack {
+}
--- c1/src/main/java/chaper2/queue.java
+++ c1/src/main/java/chaper2/queue.java
@@ -1,4 +1,39 @@
 package chaper2;
 
+import java.util.Scanner;
+
+/**
+ * 用数组模拟队列
+ *
+ * @author huijuan zheng
+ *
+ */
 public class queue {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
+
+        // 初始化队列（在队尾tt插入元素，在队头弹出元素）
+        int[] q = new int[100010];
+        int hh = 0;
+        int tt = -1;
+
+        while (m-- > 0) {
+            String s = scan.next();
+            if (s.equals("push")) {
+                int x = scan.nextInt();
+                q[++tt] = x;
+            } else if (s.equals("pop")) {
+                hh++;
+            } else if (s.equals("empty")) {
+                if (hh > tt) {
+                    System.out.println("YES");
+                } else {
+                    System.out.println("NO");
+                }
+            } else {
+                System.out.println(q[hh]);
+            }
+        }
+    }
 }
